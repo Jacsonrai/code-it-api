47baf7ffca5ab249385ecad1259c5d5144cda251
jacsonrai <jacksonrai1996@gmail.com>
2024-02-13 21:07:57 +0545

    feature added

--- server.js
+++ server.js
@@ -88,7 +88,11 @@ async(req,res)=>{
    
     try{
         const blog=await Blog.create(req.body)
-        res.status(200).json(blog)
+        res.status(200).json({
+            message:"blog created successfully",
+            status:200,
+            data:blog
+        })
     }
     catch(err){
         console.log(err.message)
